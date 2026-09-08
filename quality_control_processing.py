119db69ab7c1b099b8dd7efa760303a6b9766521
Alex <129570497+ChenSizhe13893461199@users.noreply.github.com>
2026-09-08 09:57:08 +0800

    Update quality_control_processing.py

--- quality_control_processing.py
+++ quality_control_processing.py
@@ -112,7 +112,7 @@ def calculate_judgment(rec_base_with_coord, pure_rec_base, pre_recipient_str):
     if len(pre_parts) != 3:
         return 1
     pre_base = pre_parts[0]
-    pre_coord_str = f"{pre_parts[1]}_{pre_parts[2]}"  # 合并为 "1563_6837"
+    pre_coord_str = f"{pre_parts[1]}_{pre_parts[2]}"
     pre_start, pre_end = parse_coordinate_pair(pre_coord_str, sep='_')
     if pre_start is None or pre_end is None:
         return 1
